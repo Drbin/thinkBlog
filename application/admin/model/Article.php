df45748a713f493a0c960eab9987698308d31e33
drbin <dairubin0605@163.com>
2019-04-28 17:09:40 +0800

    验证码

--- application/admin/model/Article.php
+++ application/admin/model/Article.php
@@ -14,10 +14,14 @@ class Article extends Model
 {
     protected $table= 'about_tbl';
     public function getList($page){
+        if(empty($page)){
+            $start=($page-1)*10+1;
+            $end= $page*10;
+        }
         $result=$this->field("news_id AS id,
         news_name AS name,
         news_content AS content")
-            ->limit();
+            ->limit($start,$end);
     }
     public function edit($data,$id){
         $result= $this->save(
